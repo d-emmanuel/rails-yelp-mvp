5f76345e0c11e3b4812e569f7da304a4840f426d
Dan Emmanuel dan.emmanuel85@gmail.com <dan.emmanuel85@gmail.com>
2020-11-11 15:39:21 +0100

    Finished Model Validation

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,20 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+end
